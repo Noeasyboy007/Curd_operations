95fcbc0e814022db86b6027ceb03edbc2b0b7bfb
Aritra Bera <aritrabera69@gmail.com>
2024-09-13 23:25:31 +0530

    update user

--- frontend/src/components/CreateUser.jsx
+++ frontend/src/components/CreateUser.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-
+import { } from 'react'
+import { Link } from "react-router-dom"
 const CreateUser = () => {
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
       <div className='w-50 bg-white rounded p-3'>
         <form action="">
           <h2 className='fw-bold text-md-center text-primary '>ADD USER</h2>
+          <Link to='/' className='btn btn-danger fw-bold'>HOME</Link>
           <div className='mb-2'>
             <label htmlFor="">Name</label>
             <input type='text' className='form-control' placeholder='Enter your Name' />
